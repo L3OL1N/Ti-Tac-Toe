1b33a29cf11b640d322e4183010d6e61b8d1c6e6
L3OL1N <z3650888@gmail.com>
2022-07-29 09:50:38 +0800

    Feature reset draw

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,5 @@
 let circle;
-const winCombination=[
-    ["7","8","9"],
-    ["4","5","6"],
-    ["1","2","3"],
-    ["3","5","7"],
-    ["1","5","9"],
-    ["1","4","7"],
-    ["2","5","8"],
-    ["3","6","9"]
-];
+
 function draw(turn){
     let resalt;
     let OandX = ["O","X"];
@@ -21,10 +12,13 @@ function handleClick(e){
     const currentTurn = draw(turn)
     const winDiv = document.getElementById("winner");
     placeMark(cell,currentTurn);
+    console.log(isDraw(cell));
     circle = !circle;
-    if(checkWinner(cell,currentTurn)) {
-        winDiv.children[0].innerHTML = `${currentTurn} win !!!`
+    if(isDraw(cell) || checkWinner(cell,currentTurn)) {
+        if(isDraw(cell)) winDiv.children[0].innerHTML = `Draw !!!`;
+        if(checkWinner(cell,currentTurn)) winDiv.children[0].innerHTML = `${currentTurn} win !!!`;
         winDiv.style.display = "flex";
+        winDiv.addEventListener("click",()=> window.location.reload());
     }
 }
 
@@ -34,7 +28,6 @@ function placeMark(cell,resalt){
 
 function checkWinner(cell,currentTurn){
     const combination =[];
-    const arr = Array.from(winCombination);
     const wrapCells = cell.parentNode.children;
     for(let i = 0; i < 9; i++){
         if(wrapCells[i].innerHTML === currentTurn){
@@ -42,9 +35,7 @@ function checkWinner(cell,currentTurn){
             combination.push(found);
         }
     }
-    console.log(combination);
     return combiCheck(combination);
-
 }
 function combiCheck(arr) {
     const array = arr;
@@ -62,12 +53,16 @@ function combiCheck(arr) {
     //none
     return false;
 };
+function isDraw(cell){
+    return [...cell.parentNode.children].every(cell =>{
+        return cell.innerHTML !="";
+    });
+}
 
 const blockElement = document.querySelectorAll(".block");
 //main event listener
 const main=(()=>{
     const wrapDiv = document.getElementById("wrap");
-    
     for(let i = 0; i < wrapDiv.childElementCount; i++){
         var id = wrapDiv.children[i].getAttribute("id");
         wrapDiv.children[i].style = "grid-area : "+id;
